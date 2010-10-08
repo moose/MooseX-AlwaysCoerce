c3847a761f73c1d6c3bab07973f3bc7e87995a73
Karen Etheridge <ether@cpan.org>
2010-10-08 11:18:38 -0700

    bump to version 0.10

--- lib/MooseX/AlwaysCoerce.pm
+++ lib/MooseX/AlwaysCoerce.pm
@@ -18,11 +18,11 @@ MooseX::AlwaysCoerce - Automatically enable coercions for Moose attributes
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 =head1 SYNOPSIS
 
