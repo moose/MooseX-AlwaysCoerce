c5b5774e072a14fc5f765f1358a6466ec98bbbdc
Karen Etheridge <ether@cpan.org>
2010-10-08 12:07:02 -0700

    0.11 release to fix bad tarball.

--- lib/MooseX/AlwaysCoerce.pm
+++ lib/MooseX/AlwaysCoerce.pm
@@ -18,11 +18,11 @@ MooseX::AlwaysCoerce - Automatically enable coercions for Moose attributes
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 SYNOPSIS
 
