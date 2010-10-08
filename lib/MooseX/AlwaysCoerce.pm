d62a50d8ca02e224c41911e78a285c5b8f82f1be
Karen Etheridge <ether@cpan.org>
2010-10-08 12:16:29 -0700

    bump to 0.12 to fix upload *again*

--- lib/MooseX/AlwaysCoerce.pm
+++ lib/MooseX/AlwaysCoerce.pm
@@ -18,11 +18,11 @@ MooseX::AlwaysCoerce - Automatically enable coercions for Moose attributes
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
